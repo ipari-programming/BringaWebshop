7e1e0437bfe4ad895da2bdc74244b28f63240990
Linux1230 <pekker77@gmail.com>
2020-02-11 12:52:03 +0100

    hazi elkezdve

--- WebUI/src/pages/details/details.tsx
+++ WebUI/src/pages/details/details.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { Connected } from "./../../lib/store/connected.mixin";
-import { RouteComponentProps } from "react-router";
+import { RouteComponentProps, Route } from "react-router";
 import { AppStore } from "./../../lib/appStore";
 import { Theme, createStyles, withStyles, WithStyles, TextField, Typography, Button } from "@material-ui/core"
 import withRoot from "./../../withRoot";
-import { Routes } from "../../routing/urls";
+import { Routes } from "./../../routing/urls";
 import { BicycleResponse } from "./../../services/client/bicycleService";
 import { CustomColors } from "./../../style/colors";
+import HeaderComponent from "./../header/header";
+import FooterComponent from "./../footer/footer";
 
 const styles = (theme: Theme) =>
   createStyles
@@ -16,13 +18,46 @@ const styles = (theme: Theme) =>
       display: "flex",
       flexDirection: "column",
       flexGrow: 1,
-      justifyContent: "center",
-      alignItems: "center",
       backgroundColor: CustomColors.background
     },
-    hText:
+    detailsText:
+    {
+        color: "#33ff00",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    row:
+    {
+      display: "flex",
+      flexGrow: 1,
+      flexDirection: "row"
+    },
+    imgContainer:
+    {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "50%"
+    },
+    detailsContainer:
+    {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "50%"
+    },
+    text:
     {
       color: "#33ff00"
+    },
+    img:
+    {
+        margin: 20,
+        borderRadius: 15,
+        border: "1px #000"
     }
   });
 
@@ -52,9 +87,28 @@ class Details extends Connected<typeof React.Component, IProps & WithStyles<type
 
         const Body = () =>
             <div className={css.container}>
-                <h1 className={css.hText}>Részletek</h1>
-                <h1 className={css.hText}>{this.state.bicycle.Marka}</h1>
-                 <h2 className={css.hText}>{this.state.bicycle.Tipus}</h2>
+                <h1 className={css.detailsText}>Részletek</h1>
+                <Route render={ props => <HeaderComponent {...props}/> }/>
+                <div className={css.row}>
+                    <div className={css.imgContainer}>
+                        <img className={css.img} src={this.state.bicycle.URL} />
+                        <p className={css.text}>{this.state.bicycle.Ar} HUF</p>
+                        <Button variant="contained" color="primary">
+                            Kosárba
+                        </Button>
+                    </div>
+                    <div className={css.detailsContainer}>
+                        <h3 className={css.text}>Termék neve: {this.state.bicycle.Marka} {this.state.bicycle.Tipus} bicikli</h3><br/>
+                        <h3 className={css.text}>Termék tulajdonságai:</h3>
+                            <p className={css.text}>Márka: {this.state.bicycle.Marka}</p>
+                            <p className={css.text}>Típus: {this.state.bicycle.Tipus}</p>
+                            <p className={css.text}>Felni átmérő: {this.state.bicycle.FelniAtmero}</p>
+                            <p className={css.text}>Váltó típusa: {this.state.bicycle.Valto}</p>
+                            <p className={css.text}>Váz mérete: {this.state.bicycle.Vazmeret}</p>
+                            <p className={css.text}>Cikkszám: {this.state.bicycle.Cikkszam}</p>
+                    </div>
+                </div>
+                <FooterComponent />
             </div>
 
         return Body();
